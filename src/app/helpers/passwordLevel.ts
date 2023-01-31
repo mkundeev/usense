f916b5637a923b1095277f1c58300e2780aaa625
Maksym Kundeev <mkundeev@gmail.com>
2023-01-31 22:52:22 +0200

    constants

--- src/app/helpers/passwordLevel.ts
+++ src/app/helpers/passwordLevel.ts
@@ -1,7 +1,8 @@
+import { CONST } from '../const/const';
 const passwordLevel = (password: string) => {
-  const symbols = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+  const symbols = CONST.SYMBOLS;
   const hasNumber = /\d/.test(password);
-  const hasLetter = /[a-z]/.test(password);
+  const hasLetter = /[a-z]/i.test(password);
   const hasSymbols = symbols.test(password);
   const level = Number(hasNumber) + Number(hasLetter) + Number(hasSymbols);
   return level;
